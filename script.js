85b324727c2994d097b108074a674fcf788ac0d8
Ron <martinchoyx@gmail.com>
2023-04-21 14:12:45 -0300

    Ajuste de navbar y seccion product

--- script.js
+++ script.js
@@ -7,16 +7,17 @@ window.onload = function() {
 // Inicializar el Mapa 
 
 function iniciarMap(){
-  var coord = {lat:-34.5956145 ,lng: -58.4431949};
-  var map = new google.maps.Map(document.getElementById('map'),{
+  const coord = {lat:-34.5956145 ,lng: -58.4431949};
+  const map = new google.maps.Map(document.getElementById('map'),{
     zoom: 10,
     center: coord
   });
-  var marker = new google.maps.Marker({
+  const marker = new google.maps.Marker({
     position: coord,
     map: map
   });
 }
+
 
 
 
